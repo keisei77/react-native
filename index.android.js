6d82fb752f8cbad780f44747a05a33368143c92d
DESKTOP-CG47L7C\KeiSei <keisei.jia@qq.com>
2017-07-04 22:20:21 +0800

    Remove useless code.

--- index.android.js
+++ index.android.js
@@ -13,34 +13,12 @@ import {
 } from 'react-native';
 
 import { StackNavigator } from 'react-navigation'
-
 import MainScreen from './view/pages/MainScreen'
 import ProfileScreen from './view/pages/ProfileScreen'
-import Timer from './view/pages/Timer'
-
-// export default class keisei extends Component {
-//   render() {
-//     return (
-//       <View style={styles.container}>
-//         <Text style={styles.welcome}>
-//           Welcome to React Native!
-//         </Text>
-//         <Text style={styles.instructions}>
-//           To get started, edit index.android.js
-//         </Text>
-//         <Text style={styles.instructions}>
-//           Double tap R on your keyboard to reload,{'\n'}
-//           Shake or press menu button for dev menu
-//         </Text>
-//       </View>
-//     );
-//   }
-// }
 
 export default keisei = StackNavigator({
   Main: { screen: MainScreen },
-  Profile: { screen: ProfileScreen },
-  Timer: { screen: Timer }
+  Profile: { screen: ProfileScreen }
 })
 
 const styles = StyleSheet.create({
